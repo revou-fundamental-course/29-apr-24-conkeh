fd261a3a18f53f9dc0d5b8da2f9991b235050a3f
conkeh <132237963+conkeh@users.noreply.github.com>
2024-05-08 12:12:03 -0400

    slider

--- travel_agent2/js/script.js
+++ travel_agent2/js/script.js
@@ -1,2 +1,17 @@
-var slideIndex = 1;
-showDivs(slideIndex);
+//images auto slide
+var firstIndex = 0;
+function automaticSlide() {
+  setTimeout(automaticSlide, 2000);
+  var pics;
+  const slide = document.getElementsByClassName("slide");
+  for (pics = 0; pics < slide.length; pics++) {
+    slide[pics].style.display = "none";
+  }
+  firstIndex++;
+  if (firstIndex > slide.length) {
+    firstIndex = 1;
+  }
+
+  slide[firstIndex - 1].style.display = "block";
+}
+automaticSlide();
